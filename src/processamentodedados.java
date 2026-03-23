a1009900930677bee5fa4272235a9a770fb48aa8
joao pedro <joaodev2107@gmail.com>
2026-03-23 17:17:56 -0300

    exemplos de processamento de dados

---
+++ src/processamentodedados.java
@@ -0,0 +1,43 @@
+public class processamentodedados {
+    public static void main(String[] args)  {
+        
+        // exemplo de processamento de dados 1
+
+       /*  int x, y;
+        x = 5;
+        y = 2 * x;
+        System.out.println(x);
+        System.out.println(y);
+        */
+
+        // exemplo de processamento de dados 2
+
+        /* 
+        int x;
+        double y;
+        x = 5;
+        y = 2 * x;
+        System.out.println(x);
+        System.out.println(y);
+        */
+
+         // exemplo de processamento de dados 3 atribuindo o valor de uma variável a outra
+       /* double a, b, c, area;
+        a = 6.0;
+        b = 8.0;
+        c = 5.0;
+        area = (a + b) /2.0 * c;
+        System.out.println(area);*/
+
+        // exemplo de processamento de dados 4 casting
+         
+       /* int a,b;
+        double resultado;
+        a = 5;
+        b = 2;
+        resultado =  (double) a /  b;
+        System.out.println(resultado);*/
+
+    }
+
+}
