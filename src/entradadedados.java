e807137319de44dd536321555532180c724b02fb
joao pedro <joaodev2107@gmail.com>
2026-03-24 02:55:47 -0300

    entrada de dados em java

---
+++ src/entradadedados.java
@@ -0,0 +1,49 @@
+/* import java.util.Scanner;
+
+public class entradadedados {
+    public static void main(String[] args) { 
+
+        //recebe dados do teclado
+        Scanner sc = new Scanner(System.in);
+         
+      // exemplo de entrada de dados (leitura de uma palavra)   
+     String x ;
+      x = sc.next();
+      System.out.println("vc digitou : " + x); 
+
+        // exemplo de entrada de dados (leitura de um número inteiro)
+         
+       /* int x;
+        x = sc.nextInt();
+        System.out.println("vc digitou : " + x);
+        
+        //exemplo de entrada de dados (leitura de um número flutuante)
+         
+        /* double x;
+        x = sc.nextDouble();
+        System.out.println("vc digitou: " + x );
+
+        char x;
+        x = sc.next().charAt(0);
+        System.out.println("vc digitou: " + x);
+
+        String x;
+        int y;
+        double z;
+        x = sc.next();
+        y = sc.nextInt();
+        z = sc.nextDouble();
+        System.out.println("dados digitados: ");
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+
+        //fechando o scanner para evitar vazamento de memória
+        sc.close();
+        
+    }
+
+
+
+}*/
