1692d2f47994a9f0f0049bd54eca9aa9ba74e711
joao pedro <joaodev2107@gmail.com>
2026-03-25 02:15:49 -0300

    Exercícios sobre Estrutura Sequencial (entrada, processamento, saída)

---
+++ src/exercicios.java
@@ -0,0 +1,62 @@
+/*import java.util.Scanner;
+
+public class exercicios {
+    
+    public static void main(String[]args){
+        
+        Scanner sc = new Scanner(System.in);
+ 
+       double largura, comprimento, preco, area, metro;
+
+       
+       largura = sc.nextDouble();
+       comprimento = sc.nextDouble();
+       metro = sc.nextDouble();
+        
+        
+       area = (largura * comprimento);
+       preco = (area * metro);
+
+       System.out.printf("o total da area = %.1f%n o total do preco = %.2f", area, preco);
+
+        
+         int a, b, soma;
+
+         a = sc.nextInt();
+         b = sc.nextInt();
+        
+         soma = (a + b);
+
+         System.out.printf("a soma de %d + %d = %d ",a , b , soma  );
+            
+
+        double raio, area , pi = 3.14159;
+
+        raio = sc.nextDouble();
+        area = pi * raio *raio;
+        System.out.printf("raio = %.2f area = %.4f ", raio, area );
+
+
+        System.out.print("digite o valor do raio: ");
+        double raio = sc.nextDouble();
+
+        double area = Math.PI * Math.pow(raio, 2.0);
+        double comprimento = 2.0 * Math.PI * raio ;
+
+        System.out.printf("o raio digitado foi %.2f%n ", raio );
+        System.out.printf("A área do círculo é: %.2f%n", area);
+        System.out.printf("O comprimento é: %.2f%n", comprimento);
+
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int resultado = a * b - c * d ;
+        System.out.println(resultado);
+
+         sc.close();
+
+    }
+}*/
