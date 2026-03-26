c03cc5270148a36fdf73adc35831db4a5105b764
joao pedro <joaodev2107@gmail.com>
2026-03-26 01:18:05 -0300

    Exercícios sobre Estrutura Sequencial (entrada, processamento, saída)

--- src/exercicios.java
+++ src/exercicios.java
@@ -55,6 +55,46 @@ public class exercicios {
 
         int resultado = a * b - c * d ;
         System.out.println(resultado);
+  
+        System.out.println("digite o numeros de funcionarios, seu numero de hrs trabalhado , valor por hr  ");
+
+        int func = sc.nextInt();
+        int hr = sc.nextInt();
+        double hrstrab = sc.nextDouble();
+
+        double salario = hr * hrstrab;
+
+        System.out.printf("o seu salario foi: %.2f", salario );
+
+        int codp = sc.nextInt();
+        int nump = sc.nextInt();
+        
+        double valorp1 = sc.nextDouble();
+
+        int codp2 = sc.nextInt();
+        int nump2 = sc.nextInt();
+       
+        
+        double valorp2 = sc.nextDouble();
+        
+
+        double total =  ( nump * valorp1) + (nump2 * valorp2);
+
+        System.out.printf("valor a pagar %.2f%n", total);
+
+        double pi = 3.14;
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        double area = (a * c) / 2;
+         double areacirculo = pi * c * c;  
+         double trapezio = (a + b) * c /2;
+         double quadrado = b * 4;
+         double retangulo = a * b;
+
+
+         System.out.printf("valor da area: %.3f%n area do circulo: %.3f%n area do trapezio: %.3f%n area do quadrado: %.3f%n area do retangulo: %.3f%n ", area, areacirculo, trapezio, quadrado, retangulo);
 
          sc.close();
 
