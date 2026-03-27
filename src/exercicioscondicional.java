e782a45868e2904f4875cdc2d00e6ff4c5c9b273
joao pedro <joaodev2107@gmail.com>
2026-03-27 03:02:17 -0300

    exercicios de estruturacondicional

---
+++ src/exercicioscondicional.java
@@ -0,0 +1,67 @@
+
+ /*   import java.util.Scanner; // Importa a classe Scanner para permitir a entrada de dados pelo teclado
+
+public class exercicioscondicional {
+    
+    public static void main(String[] args) {
+       
+        // Cria um objeto 'sc' do tipo Scanner para ler o que o usuário digitar
+        Scanner sc = new Scanner(System.in);
+ 
+        System.out.println("digite um numero: ");
+
+        int num = sc.nextInt();
+
+        if(num >= 0){
+            System.out.println("numero positivo");
+
+        }
+        else{
+            System.out.println("numero negativo");
+        }
+        
+        
+        System.out.println("digite um numero:");
+        int num = sc.nextInt();
+
+        if(num % 2 == 0){
+                System.out.println("numero e par");
+        }
+        else{
+            System.out.println("numero e impar");
+        }
+
+        
+        System.out.println("informe o valor de a e b: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a % b == 0 || b % a == 0){
+            System.out.println("sao mutiplos");
+        }
+        else{
+            System.out.println("nao sao mutiplos");
+        }
+
+     
+        System.out.println("informe dois valores: ");
+        int hrinicial = sc.nextInt();
+        int hrfinal = sc.nextInt();
+        int duracao;
+
+        if(hrinicial< hrfinal){
+            
+            duracao = hrfinal - hrinicial;
+        }
+        else{
+            duracao = 24 - hrinicial + hrfinal;
+        }
+
+        System.out.println("o jogo durou " + duracao + "horas");
+        
+        // Fecha o Scanner para liberar os recursos de memória (boa prática)
+        sc.close();
+    }
+}
+    
+*/
