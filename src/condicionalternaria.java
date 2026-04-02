08f45970546585a7723be63ef3620a096781d1db
joao pedro <joaodev2107@gmail.com>
2026-04-02 03:05:22 -0300

    Corrigindo erro no exercicio 3 e organizando arquivos

---
+++ src/condicionalternaria.java
@@ -0,0 +1,28 @@
+/*import java.util.Scanner;
+
+public class condicionalternaria {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     
+           int x = 10;
+        String resultado = (x > 5) ? "maior que 5" : "menor ou igual a 5";
+        System.out.println(resultado);
+        
+        
+        double preco = scanner.nextDouble();
+        double desconto ;
+        if (preco < 100){
+            desconto = preco * 0.1;
+        } else {
+            desconto = preco * 0.5;
+        }
+        System.out.println("Desconto: " + desconto);
+
+        double preco = 34.5;
+        double desconto = (preco < 20) ? preco * 0.1 : preco * 0.5;   
+        System.out.println("Desconto: " + desconto);
+        sc.close();
+
+    }
+}*/
+ 
--- src/exercicioscondicional.java
+++ src/exercicioscondicional.java
@@ -1,5 +1,5 @@
-
- /*   import java.util.Scanner; // Importa a classe Scanner para permitir a entrada de dados pelo teclado
+/* 
+    import java.util.Scanner; // Importa a classe Scanner para permitir a entrada de dados pelo teclado
 
 public class exercicioscondicional {
     
@@ -7,7 +7,7 @@ public class exercicioscondicional {
        
         // Cria um objeto 'sc' do tipo Scanner para ler o que o usuário digitar
         Scanner sc = new Scanner(System.in);
- 
+
         System.out.println("digite um numero: ");
 
         int num = sc.nextInt();
@@ -58,10 +58,121 @@ public class exercicioscondicional {
         }
 
         System.out.println("o jogo durou " + duracao + "horas");
-        
+
+
+        int cod = sc.nextInt();
+        int qtd = sc.nextInt();
+
+        double total;
+
+        if(cod == 1){
+            total = qtd * 4.00;
+        }
+        else if(cod == 2){
+            total = qtd * 4.50;
+        }
+        else if(cod == 3){
+            total = qtd * 5.0;
+        }
+        else if (cod == 4) {
+            total = qtd * 2.00;
+        } 
+        else if (cod == 5) {
+            total = qtd * 1.50;
+        }
+        else {
+            total = 0;
+            System.out.println("codigo invalido");
+        }
+
+        if(total > 0){
+            System.out.printf("total: %.2f%n", total);
+        }
+
+ 
+
+
+        System.out.println("digite um valor");
+        double valor = sc.nextDouble();
+
+        if (valor < 0 || valor > 100){
+            System.out.println("fora de intervalo");
+        }
+        else if(valor <= 25.0){
+            System.out.println("intervalo [0 , 25]");
+        }
+        else if(valor <= 50.0){ 
+            System.out.println("intervalo (25,50]");
+        }
+        else if(valor <= 75.0){
+            System.out.println("intervalo (50,75");
+        }
+        else{
+            System.out.println("intervalo 75, 100");
+        }
+
+
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+
+        if(x == 0.0 && y == 0.0){
+            System.out.println("origem");
+        }
+        // 2. Verificamos se está sobre o Eixo X (Y é zero, mas X não)
+        else if (y == 0.0) {
+            System.out.println("Eixo X");
+        }
+        // 3. Verificamos se está sobre o Eixo Y (X é zero, mas Y não)
+        else if (x == 0.0) {
+            System.out.println("Eixo Y");
+        }
+        // 4. Verificamos os Quadrantes baseados nos sinais (+ ou -)
+        else if (x > 0.0 && y > 0.0) {
+            System.out.println("Q1");
+        }
+        else if (x < 0.0 && y > 0.0) {
+            System.out.println("Q2");
+        }
+        else if (x < 0.0 && y < 0.0) {
+            System.out.println("Q3");
+        }
+        else {
+            // Se não é nenhum dos anteriores, só pode ser Q4 (x > 0 e y < 0)
+            System.out.println("Q4");
+        }
+       
+       
+        double salario = sc.nextDouble();
+        double imposto;
+
+        if (salario <= 2000.0) {
+            imposto = 0.0;
+        } 
+        else if (salario <= 3000.0) {
+            // Paga 8% sobre o que exceder 2000
+            imposto = (salario - 2000.0) * 0.08;
+        } 
+        else if (salario <= 4500.0) {
+            // Paga 8% sobre os 1000 da segunda faixa + 18% sobre o que exceder 3000
+            imposto = (1000.0 * 0.08) + (salario - 3000.0) * 0.18;
+        } 
+        else {
+            // Paga 8% sobre a 2ª faixa + 18% sobre a 3ª faixa + 28% sobre o que exceder 4500
+            imposto = (1000.0 * 0.08) + (1500.0 * 0.18) + (salario - 4500.0) * 0.28;
+        }
+
+        if (imposto == 0.0) {
+            System.out.println("Isento");
+        } else {
+            // %f com .2 para garantir as duas casas decimais
+            System.out.printf("R$ %.2f%n", imposto);
+        }
+
+ 
+
         // Fecha o Scanner para liberar os recursos de memória (boa prática)
         sc.close();
     }
 }
-    
-*/
+   */
+
